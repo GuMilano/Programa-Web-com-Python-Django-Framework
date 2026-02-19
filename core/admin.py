2c519a80811db9b0343216663e83d4eb32feeea5
GuMilano <gustavo.fenex@hotmail.com>
2026-02-19 15:58:44 -0300

    add produtos/clientes

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Produto, Cliente
+
+
+class produtoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "descricao", "quantidade", "preco")
+    search_fields = ("nome", "descricao")
+
+class clienteAdmin(admin.ModelAdmin):
+    list_display = ("nome", "sobrenome", "email", "telefone")
+    search_fields = ("nome", "sobrenome", "email")
+
+
+admin.site.register(Produto, produtoAdmin)
+admin.site.register(Cliente, clienteAdmin)
--- core/models.py
+++ core/models.py
@@ -4,10 +4,17 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
+    quantidade = models.IntegerField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     sobrenome = models.CharField(max_length=100)
     email = models.EmailField()
     telefone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
