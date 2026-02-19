9150ace706977e5747c4ef649f1f74be001557b3
GuMilano <gustavo.fenex@hotmail.com>
2026-02-19 13:36:10 -0300

    add models e migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
