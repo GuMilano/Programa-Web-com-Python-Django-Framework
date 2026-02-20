693079dbc9647f58ace28ba148500f9556cdf29a
GuMilano <gustavo.fenex@hotmail.com>
2026-02-20 08:29:27 -0300

    teste shell django

--- core/views.py
+++ core/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    print(dir(request))
+    print(f"User: {request.user}")
+
+    if str(request.user) == "AnonymousUser":
+        teste = "Usuário não logado"
+    else:
+        teste = "Usuário logado"
     context = {
         "curso": "Programação Web com Django Framework",
+        "logado": teste
     }
     return render(request, 'index.html', context)
 
